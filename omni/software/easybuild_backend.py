aed5f51279a472a9e66a5db4d4ebe5272f8d4194
Izaskun Mallona <izaskun.mallona@gmail.com>
2024-08-07 15:26:09 +0200

    Update runner's modulepath

--- omni/software/easybuild_backend.py
+++ omni/software/easybuild_backend.py
@@ -23,10 +23,8 @@ HOME=op.expanduser("~")
 # opts, _ = set_up_configuration(args=[], silent=True)
 set_up_configuration(args=['--quiet'], silent=True)
 
-
 ## shell-based stuff, partly to be replaced by direct eb API calls -------------------------------------
 
-    
 def generate_default_easybuild_config_arguments(workdir):
     # modulepath = op.join(workdir, 'easybuild', 'modules', 'all')
     # buildpath = op.join(workdir, 'easybuild', 'build')
@@ -72,33 +70,14 @@ def easybuild_easyconfig(easyconfig,
     else:
         return("LOG easybuild: \n{}\n".format(output))
 
-## shell stuff end ------------------------------------------------------------------------------------
-
-def list_all_easyconfigs(benchmark_yaml):
-    return('not_implemented, perhaps import from workflow/schema?')
-
-# def easybuild_all_easyconfigs(benchmark_yaml, workdir, threads, containerize = False,
-#                               container_build_image = False):
-#     """
-#     Iterates over all easyconfigs from a benchmark yaml and easybuilds them all
-#     """
-
-#     easyconfigs = list_all_easyconfigs()
-#     for eb in easyconfigs:
-#         easybuild_easyconfig(easyconfig = eb, workdir = workdir, threads = threads,
-#                              containerize = containerize, container_build_image = container_build_image)
-
-# not needed, the yaml will have both
-# def get_easyconfig_from_envmodule_name(envmodule):
-#     return('not implemented')
 
 def parse_easyconfig(ec_fn, workdir):
     """
     Find and parse an easyconfig with specified filename,
     and return parsed easyconfig file (an EasyConfig instance).
     """
-    opts, _ = set_up_configuration(args = generate_default_easybuild_config_arguments(workdir).split(),
-                                   silent = True)    
+    # opts, _ = set_up_configuration(args = generate_default_easybuild_config_arguments(workdir).split(),
+    #                                silent = True) 
     
     ec_path = det_easyconfig_paths([ec_fn])[0]
     
@@ -116,63 +95,20 @@ def get_easyconfig_full_path(easyconfig, workdir):
         return(ec_path)
     except:
         raise FileNotFoundError('ERROR: easyconfig not found.\n')
-        # sys.exit('Premature exit')
 
-    
+
 def get_envmodule_name_from_easyconfig(easyconfig, workdir):
-    # export_lmod_env_vars()
     ec_path, ec = parse_easyconfig(easyconfig, workdir)
     return(os.path.join(ec['name'], det_full_ec_version(ec)))
 
-# # 0 success, there is a module tool
-# # 1 error, there is none
-# def check_module_tool():
-#     mod_tool = modules_tool()
-#     if mod_tool.NAME is None:
-#         return(1) #'Module tool: not installed')
-#     else:
-#         return(0) # "Module tool: %s version %s" % (mod_tool.NAME, mod_tool.version))
-
-# def list_available_modules(mod_name):
-#     modules_tool().use(op.join(HOME, '.local', 'easybuild'))
-#     return(modules_tool().available(mod_name = mod_name))
-
-        
-# def check_envmodule_status(envmodule):
-#     mod_tool = modules_tool()
-#     if len(mod_tool.available(envmodule)) == 0:
-#         return('not installed')
-#     else:
-#         return('installed')
-
-# # beware containerpath hardcoded to default
-# def get_toolchain_container_path(toochain,
-#                                  containerpath = op.join(HOME, '.local', 'easybuild', 'containers')):
-#     return(op.join(containerpath, toolchain + '.sif'))
-                                      
-
-# def pull_base_container():
-#     cmd = "singularity build --sandbox debian_trixie_slim.sif docker://debian:trixie-slim"
-
-#     try:
-#         output = subprocess.check_output(
-#             cmd, stderr = subprocess.STDOUT, shell = True,
-#             universal_newlines = True)
-#     except subprocess.CalledProcessError as exc:
-#         return("ERROR pulling failed %s %s:" %(exc.returncode, exc.output))
-#     else:
-#         return("LOG pulling: \n{}\n".format(output))
-    
 
 def build_easybuild_easyconfig_command(easyconfig,
                                        workdir,
                                        threads,
                                        containerize = False,
                                        container_build_image = False):
-
     
-    args = generate_default_easybuild_config_arguments(workdir = workdir)
-    
+    # args = generate_default_easybuild_config_arguments(workdir = workdir)    
     cmd = """eb %(easyconfig)s --robot --parallel=%(threads)s \
               --detect-loaded-modules=unload --check-ebroot-env-vars=unset""" %{
                   'easyconfig' : easyconfig,
@@ -222,7 +158,3 @@ def singularity_push(sif, docker_username, docker_password, oras):
         return("ERROR singularity build failed:", exc)
     else:
         return("DONE.")
-
-# def load_module_api(mod_name):
-#     modules_tool().use(op.join(HOME, '.local', 'easybuild'))
-#     modules_tool().load(modules = [mod_name], purge = False)
